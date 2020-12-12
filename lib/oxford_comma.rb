52ca2d83f8ddfe24042bc4d5c0f12ffe9aba420c
Antonio <atcarter005@gmail.com>
2020-12-12 14:42:11 -0600

    commit tree 492882d64e14184dad3a464c749a76b4b3241a48

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+    if array.size > 2
+        string = array.join(", ")
+        string.insert(-array[-1].size - 1, "and ")
 
+    elsif array.size > 1
+        array.join(" and ")
+
+    else
+        array.join
+    end
 end
